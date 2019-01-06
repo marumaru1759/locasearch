6ab7584fd460063ccb482e7384ff88e7873b7221
marumaru1759 <marum1759@gmail.com>
2019-01-06 08:09:12 +0000

    server config

--- src/components/CustomeWel.vue
+++ src/components/CustomeWel.vue
@@ -15,7 +15,7 @@
         	<h1 class="cover-heading">ロケサーチ（仮）</h1>
         	<p class="lead">あなたの行きたい場所を見つけるサイト</p>
         	<p class="lead">
-          	<router-link toh="/register" class="btn btn-lg btn-secondary">新規登録</a>
+          	<router-link to="/register" class="btn btn-lg btn-secondary">新規登録</a>
         	</p>
         </main>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import CustomeWel from '@/components/CustomeWel'
 import routes from './routes'
 
 

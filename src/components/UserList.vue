6eefdebf7d67cc3e1f381d9b55078af439536d13
marumaru1759 <marum1759@gmail.com>
2019-01-17 21:48:30 +0900

    test
    test

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -3,7 +3,7 @@
    <div id="user-list">
    	<h1 class="midlang">UserList</h1>
    </div>
-   <div v-for="user in userData" :key="user.id">
+   <div v-for="user in users" :key="user.id">
    	<h2 class="midlang"> Hi {{ user.name }} </h2>
    </div>
 </div>

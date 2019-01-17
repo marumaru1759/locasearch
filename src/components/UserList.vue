65f1bfa2d1c9c6303323e46cc4b8c6aa83d6f9a9
marumaru1759 <marum1759@gmail.com>
2019-01-17 20:57:00 +0900

    UserListVue merged
    UserList Vue merged

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,11 +1,65 @@
 <template>
    <div id="user-list">UserList</div>
    <div v-for="user in users": key="user.id">
-   	<h2> {{ user.name}} </h2>
+   	<h2> {{ user.name }} </h2>
    </div>
 </template>
 
-<script src="./showlist.js"></script>
+<script>
+	var userData = [
+{
+	id:1,
+	name: 'Takuya Tajima',
+	Password: '123456'
+},{
+	id:2,
+	name: 'Yohei Noda',
+	Password: '123456'
+}
+]
+
+var getUsers = function(callback){
+	setTimeoout(function(){
+		callback(null, userData)
+	},1000)
+}
+
+var UserList = {
+	template: '#user-list',
+	data: function(){
+		return{
+			loading: false,
+			users: function(){
+				return[]
+			},
+			error: null
+		}
+	},
+
+	created: function (){
+		this.fetchData()
+	},
+
+	watch: {
+		'$route': 'fetchData'
+	},
+
+	methods: {
+		fetchData: function(){
+			this.loading = true
+			getUsers((function(err,users){
+				this.loading = false
+				if(err){
+					this.error = err.toString()
+				}else{
+					this.users = users
+				}
+			}).bind(this))
+		}
+	}
+}
+
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 

9b723da2da9919b70b312bdb0217b401d9a4c8f4
marumaru1759 <marum1759@gmail.com>
2019-01-25 20:17:00 +0900

    dd
    dd

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,12 +1,28 @@
 import * as types from './mutation-types'
 import { Auth, List, Task } from '../api'
 
-/*export default{
+export default{
+	userRegister (state, { name, password }){
+			state.userData.push({
+				id: state.nextUserId,
+				name,
+				password,
+				//done: false
+			})
+			state.nextUserd++
+		},
+
+		// restore state
+	restore (state, { userData, nextUserId}){
+			state.userData = userData
+			state.nextUserId = nextUserId
+		},
+		
 	login: ({ commit }, authInfo) => {
 		return Auth.login(authInfo)
 		.then(({ token, userId }) => {
 			commit(types.AUTH_LOGIN, { token, userId })
 		})
 		.catch(err => { throw err })
-	},
-}*/
+	}
+}

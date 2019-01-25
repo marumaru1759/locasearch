109ac356dab1e0e682c5c25782edb1ad4891fa52
marumaru1759 <marum1759@gmail.com>
2019-01-25 20:31:29 +0900

    meme
    meme

--- src/store/actions.js
+++ src/store/actions.js
@@ -17,12 +17,30 @@ export default{
 			state.userData = userData
 			state.nextUserId = nextUserId
 		},
-		
+
 	login: ({ commit }, authInfo) => {
 		return Auth.login(authInfo)
 		.then(({ token, userId }) => {
 			commit(types.AUTH_LOGIN, { token, userId })
 		})
 		.catch(err => { throw err })
+	},
+
+	//save "state" in local storage
+	save ({ state }){
+			const data ={
+				userData: state.userData,
+				nextUserId: state.nextUserId
+			}
+			localStorage.setItem('User-app-Data', JSON.stringify(data))
+		},
+
+		//resotore "state" from local storage
+	/*restore({ commit }){
+			const data = localStorage.getItem('User-app-Data')
+			if(data){
+				commit('restore', JSON.parse(data))
+			}
+		}*/
+		
 	}
-}

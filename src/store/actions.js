46314744f664c87209d249558fc3e359181ae296
marumaru1759 <marum1759@gmail.com>
2019-01-25 10:35:02 +0900

    login form has been implemented
    login basis

---
+++ src/store/actions.js
@@ -0,0 +1,9 @@
+import * as types from './mutation-types'
+import { Auth, List, Task } from '../api'
+
+export default{
+	login: {{ commit }} => {
+		// Todo:
+		throw new Error('login action should be implemented')
+	}
+}

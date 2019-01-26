bea2482fe399c71d8175475af96dfdc0af933cff
marumaru1759 <marumaru1759@gmail.com>
2019-01-26 00:24:17 +0000

    firebase auth

---
+++ src/api/firebase_auth.js
@@ -0,0 +1,7 @@
+var admin = require("firebase-admin");
+var serviceAccount = require("../../private/locasearch-firebase-adminsdk-a461o-0f31d0744e.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://locasearch.firebaseio.com"
+});

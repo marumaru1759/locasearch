6e996749ee4b6cfc8eb674ca584af7afd82d1bb7
marumaru1759 <marum1759@gmail.com>
2019-01-29 22:34:02 +0900

    firebase auth initialization
    firebase auth initialization

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -2,6 +2,17 @@ import * as types from './mutation-types'
 
 
 const firebase = require("firebase");
+const config = {
+	apiKey: "AIzaSyBTtRuNAeLj9RdkYz5XpsC7u579QqLAmpg",
+    authDomain: "locasearch.firebaseapp.com",
+    databaseURL: "https://locasearch.firebaseio.com",
+    projectId: "locasearch",
+    storageBucket: "locasearch.appspot.com",
+    messagingSenderId: "660314635299"
+};
+
+firebase.initializeApp(config);
+
 
 export default {
 	[types.AUTH_LOGIN] (state, payload) {

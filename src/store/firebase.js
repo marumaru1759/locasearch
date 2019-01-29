a4e3ea32141ce1f286b01e7709871c9d02f385d7
marumaru1759 <marum1759@gmail.com>
2019-01-30 00:20:28 +0900

    d
    dd

--- src/store/firebase.js
+++ src/store/firebase.js
@@ -1,11 +1,10 @@
 import * as firebase from 'firebase';
 
-
 const config = {
 	apiKey: "AIzaSyBTtRuNAeLj9RdkYz5XpsC7u579QqLAmpg",
     authDomain: "locasearch.firebaseapp.com",
     databaseURL: "https://locasearch.firebaseio.com",
-    projectId: "locaksearch",
+    projectId: "locasearch",
     storageBucket: "locasearch.appspot.com",
     messagingSenderId: "660314635299"
 };

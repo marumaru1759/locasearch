b087875f1c9cb1e63f297bc4e0e8329365d47276
marumaru1759 <marum1759@gmail.com>
2019-01-31 21:00:52 +0900

    getCurrentUser
    getCurrentUser

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -6,13 +6,17 @@ export default {
 		state.auth = payload
 	},
 
-	userRegister (state, { email, password }){
+	userRegister(state, { email, password }){
 		fb.firebaseapp.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
   			// Handle Errors here.
   var errorCode = error.code;
   var errorMessage = error.message;
   // ...
 });
+	}
+
+	getCurrentUser(){
+		return fb.firebaseapp.auth().currentUser;
 	}
 
 }

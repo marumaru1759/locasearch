edcad4a5316674bcadaa3a910ce76d7eebd27fe3
marumaru1759 <marum1759@gmail.com>
2019-02-06 00:05:54 +0900

    Update auth.js
    dd

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,11 +3,20 @@ import * as fb from './firebase'
 
 export default {
 	login(authInfo){
-		fb.firebaseapp.auth().signInWithEmailAndPassword(authInfo.email, authInfo.password).then( user => {
+		fb.firebaseapp.auth().signInWithEmailAndPassword(authInfo.email, authInfo.password).catch(function(error){
+		var errorCode = error.code;
+		console.log(errorCode);
+		var errorMessage = error.Message;
+		console.log(errorMessage);
+	})
+	}
+}
+
+
+/*
+.then( user => {
 						state.currentUser.uid =  fb.firebaseapp.auth().currentUser.uid; 
 						console.log(state.currentUser.uid);
 						state.currentUser.email = fb.firebaseapp.auth().currentUser.email;
 						console.log(state.currentUser.email);		
-		})
-	}
-}
+		})*/

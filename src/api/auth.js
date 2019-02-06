0d042466430b629dffb1f1e1637868f7c02533a3
marumaru1759 <marum1759@gmail.com>
2019-02-06 21:55:55 +0900

    Update auth.js
    koko

--- src/api/auth.js
+++ src/api/auth.js
@@ -5,11 +5,7 @@ export default {
 	login(authInfo){
 		console.log(authInfo.email);
 		console.log(authInfo.password);
-		fb.firebaseapp.auth().signInWithEmailAndPassword(authInfo.email, authInfo.password).catch(function(error){
-		var errorCode = error.code;
-		console.log(errorCode);
-		var errorMessage = error.Message;
-		console.log(errorMessage);
+		fb.firebaseapp.auth().signInWithEmailAndPassword(authInfo.email, authInfo.password);
 	})
 	}
 }

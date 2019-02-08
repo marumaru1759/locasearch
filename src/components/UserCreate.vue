cd5293d806e486c1028a8630075f0148e9774b12
marumaru1759 <marum1759@gmail.com>
2019-02-08 22:31:36 +0900

    Update UserCreate.vue
    dd

--- src/components/UserCreate.vue
+++ src/components/UserCreate.vue
@@ -47,7 +47,7 @@ methods:{
 			email: this.newemail,
 			password: this.newPass
 		}).then(function(){
-			if(this.newemail === this.$store.state.currentUser.email){
+			if( newemail === this.$store.state.currentUser.email){
 				this.newemail = '';
 				this.newPass = '';
 				console.log("Success");
@@ -60,71 +60,7 @@ methods:{
 		})
 	}
 
-	/*toggleRegistrationStatus(user){
-		// 'toggleRegistrationStatus' mutation is committed here
-		this.$store.commit('toggleRegistrationStatus',{
-			id: user.id
-		})
-	}*/
-	}
-}
-
-/*var postUser = function (params, callback){
-	setTimeout(function (){
-		params.id = userData.length + 1
-		userData.push(params)
-		callback(null, params)
-	}, 1000)
-}
-
-var UserCreate ={
-	template: '#user-create',
-	data: function(){
-		return{
-			sending: false,
-			user: this.defaultUser(),
-			error: null
-		}
-	},
-
-	created: function(){
-
-	},
-
-	methods:{
-		defaultUser: function(){
-			return{
-				name: '',
-				description: ''
-			}
-		},
-
-		createUser: function(){
-			if (this.user.name.trim() === ''){
-				this.error = 'Name is mandatory'
-				return
-			}
-			if (this.user.description.trim() === ''){
-				this.error = 'Descripiton is mandatory'
-				return
-			}
-
-			postUser(this.user, (function(err, user){
-				this.sending = false
-				if(err){
-					this.error = err.toString()
-				} else{
-					this.error = null
-					this.user = this.defaultUser()
-					alert('A new user has been registered')
-					this.$router.push('/')
-				}
-			}).bind(this))
-		}
-	}
-}
-
-*/
+	
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
